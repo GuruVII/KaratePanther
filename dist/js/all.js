5dd301f602e2a9650591aa2d3db23223d4b19217
GuruVII <guruvii@gmail.com>
2016-08-22 00:05:26 +0200

    minor fixed, on lower screen sizes the dropdown menu now closes and the active webpage is shown

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,3 +1,11 @@
+$(document).on('click',function(){
+$('.collapse').collapse('hide');
+}) //A sloppy workaround that hides the dropdown menu when no longer used.
+
+$("#navbar").find("li").on("click", function(){
+$("#navbar").find("li").removeClass("active");
+$(this).addClass("active");
+});
 
 app.config(function($stateProvider, $urlRouterProvider){
 
